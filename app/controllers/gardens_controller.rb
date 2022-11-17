6aacd2346c472e59f4812c08c4dbf64e7b777b34
Francesca Santoriello <f.santoriello@hotmail.com>
2022-11-17 13:43:10 +0100

    gardens actions

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,13 @@
 class GardensController < ApplicationController
+  # As a user I can see all gardens
+  # 'localhost/gardens'
+  def index
+    @gardens = Garden.all
+  end
+
+  # As a user I can see details on one garden
+  # 'localhost/gardens/:id'
+  def show
+    @garden = Garden.find(params[:id])
+  end
 end
